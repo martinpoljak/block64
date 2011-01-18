82331455c58f885a76bffed40e7b6684565c0e03
Martin Kozák <martinkozak@martinkozak.net>
2011-01-18 10:20:27 +0100

    tabs

--- lib/block64.rb
+++ lib/block64.rb
@@ -6,71 +6,72 @@ include PKey
 
 class RSA
 
-  BIT_MATCHER = /(\d+) bit/
+
+    BIT_MATCHER = /(\d+) bit/
 	
-  @size
-  @encrypt_block_size
-  @decrypt_block_size
+    @size
+    @encrypt_block_size
+    @decrypt_block_size
 	
-  # Read the length of the private key.
-  def size
-    if @size.nil?
-      @size = self.class::BIT_MATCHER.match(self.to_text)[1].to_i
-    end
-    
-    return @size
-  end
+    # Read the length of the private key.
+    def size
+        if @size.nil?
+            @size = self.class::BIT_MATCHER.match(self.to_text)[1].to_i
+        end
 
-  # Calculate the block size when encrypting.
-  def encrypt_block_size
-    if @encrypt_block_size.nil?
-      @encrypt_block_size = (self.size / 8) - 14
+        return @size
     end
-    
-    return @encrypt_block_size
-  end
 
-  # Calculate the block size when decrypting.
-  def decrypt_block_size
-    if @decrypt_block_size.nil?
-        @decrypt_block_size = (self.size / 8) - 1
+    # Calculate the block size when encrypting.
+    def encrypt_block_size
+        if @encrypt_block_size.nil?
+            @encrypt_block_size = (self.size / 8) - 14
+        end
+        
+        return @encrypt_block_size
     end
-    
-    return @decrypt_block_size
-  end
 
-  # Encrypt and base64 encode data of arbitrary length.
-  def encrypt64(str)
-    enc = ''
-    str = str.dup
-    encrypt_block_size = self.encrypt_block_size
+    # Calculate the block size when decrypting.
+    def decrypt_block_size
+        if @decrypt_block_size.nil?
+            @decrypt_block_size = (self.size / 8) - 1
+        end
     
-    while str.length > encrypt_block_size
-      enc << self.public_encrypt(str[0..encrypt_block_size])
-      str.replace(str[encrypt_block_size + 1..-1])
-    end 
-    
-    if str.length > 0
-      enc << self.public_encrypt(str[0..encrypt_block_size])
+        return @decrypt_block_size
     end
-    
-    return Base64.encode64(enc)
-  end
 
-  # Decrypt and base64 decode data of arbitrary length.
-  def decrypt64(str)
-    dec = ''
-    str = Base64.decode64(str)
-    decrypt_block_size = self.decrypt_block_size
+    # Encrypt and base64 encode data of arbitrary length.
+    def encrypt64(str)
+        enc = ''
+        str = str.dup
+        encrypt_block_size = self.encrypt_block_size
     
-    while str.length > 0
-      dec << self.private_decrypt(str[0..decrypt_block_size])
-      if str.length > decrypt_block_size
-        str.replace(str[decrypt_block_size + 1..-1])
-      end
+        while str.length > encrypt_block_size
+            enc << self.public_encrypt(str[0..encrypt_block_size])
+            str.replace(str[encrypt_block_size + 1..-1])
+        end 
+    
+        if str.length > 0
+            enc << self.public_encrypt(str[0..encrypt_block_size])
+        end
+    
+        return Base64.encode64(enc)
     end
+
+    # Decrypt and base64 decode data of arbitrary length.
+    def decrypt64(str)
+        dec = ''
+        str = Base64.decode64(str)
+        decrypt_block_size = self.decrypt_block_size
     
-    return dec
-  end
+        while str.length > 0
+            dec << self.private_decrypt(str[0..decrypt_block_size])
+            if str.length > decrypt_block_size
+                str.replace(str[decrypt_block_size + 1..-1])
+            end
+        end
+    
+        return dec
+    end
   
 end
